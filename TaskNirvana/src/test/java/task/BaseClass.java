ada02cf9457099e182c60fafb0134f1a05014157
Priyanka Katare <priyankakatare.2014@gmail.com>
2024-09-12 23:09:27 +0530

    test passed

--- TaskNirvana/src/test/java/task/BaseClass.java
+++ TaskNirvana/src/test/java/task/BaseClass.java
@@ -1,31 +1,58 @@
 package task;
 
-import java.util.concurrent.TimeUnit;
-
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.openqa.selenium.TakesScreenshot;
+import java.io.File;
+import org.testng.annotations.BeforeTest;
+import java.io.IOException;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import utility.utility;
+
 
 public class BaseClass {
-	 public WebDriver driver;
-	    public String baseUrl = "https://www.saucedemo.com";
-
-	    @SuppressWarnings("deprecation")
-		@BeforeClass
+	
+	ExtentReports extent = new ExtentReports();
+	ExtentSparkReporter spark = new ExtentSparkReporter("target/Spark.html");
+	 
+        public WebDriver driver;
+        
+	
+		@BeforeTest
 	    public void setUp() {
-	        
-	        System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe"); 
+			
+			spark.config().setTheme(Theme.DARK);
+			spark.config().setDocumentTitle("MyReport");
+	        extent.attachReporter(spark);
+            WebDriverManager.chromedriver().setup();
+	        //System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe"); 
 	        driver = new ChromeDriver(); 
-	        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	        driver.get(baseUrl);
+	        driver.get("https://www.saucedemo.com/");
 	    }
+		
+		@AfterMethod
+		
+		public void getscreenshot(ITestResult result) throws IOException {
+			if(ITestResult.FAILURE==result.getStatus()){
+				utility.TakeScreenshots(driver,result.getTestName());
+			}	
+		}
 
-	    @AfterClass
+	    @AfterTest
 	    public void tearDown() {
-	        
+	    	extent.flush();
 	          driver.quit();
-	        }}
+	        }
+}
 	    
 
 
--- TaskNirvana/src/test/java/task/LoginPage.java
+++ TaskNirvana/src/test/java/task/LoginPage.java
@@ -5,10 +5,16 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+
 public class LoginPage extends BaseClass {
 	@Test
-	public void lauch() {
-		driver.get(baseUrl);
+	public void valid() {
+		ExtentTest test=extent.createTest("verify the page valid data").assignAuthor("priyanka").assignCategory("functional test cases")
+				.assignDevice("Windows");
+		
+		//driver.get("https://www.saucedemo.com/");
 		
 		WebElement usernameField = driver.findElement(By.id("user-name")); 
         WebElement passwordField = driver.findElement(By.id("password")); 
@@ -20,9 +26,50 @@ public class LoginPage extends BaseClass {
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/inventory.html"); 
         WebElement userProfile = driver.findElement(By.className("//div[@class=\"app_logo\"]")); 
         Assert.assertTrue(userProfile.getText().contains("Swag Labs"));
+        test.pass("verify the user");
     }
+@Test
+public void invalid() {
+	ExtentTest test=extent.createTest("verify the invalid data");
 
-	}
+	
+	//driver.get("https://www.saucedemo.com/");
+	WebElement usernameField = driver.findElement(By.id("user-name")); 
+    WebElement passwordField = driver.findElement(By.id("password")); 
+    WebElement loginButton = driver.findElement(By.id("login-button")); 
+    
+    
+    usernameField.sendKeys("wrong_user");
+    passwordField.sendKeys("wrong_password");
+    loginButton.click();
+
+    WebElement errorMessage = driver.findElement(By.xpath("//div[@class=\"error-message-container error\"]")); 
+    String actualMessage = errorMessage.getText();
+    test.fail("verify msg");
+
+    Assert.assertEquals(actualMessage, "Epic sadface: Username and password do not match any user in this service"); 
+}
+@Test
+public void testEmptyFields() {
+    
+	ExtentTest test=extent.createTest("verify the empty fields");
+
+    WebElement usernameField = driver.findElement(By.id("username"));
+    WebElement passwordField = driver.findElement(By.id("password"));
+    WebElement loginButton = driver.findElement(By.id("loginButton"));
+
+    usernameField.clear(); 
+    passwordField.clear();
+    loginButton.click();
+
+    WebElement errorMessage = driver.findElement(By.xpath("//div[@class=\"error-message-container error\"]")); 
+    String actualMessage = errorMessage.getText();
+
+    Assert.assertEquals(actualMessage, "Epic sadface: Username and password do not match any user in this service");
+    test.info("Error msg is correct");
+    };
+
+}
 	
 
 
